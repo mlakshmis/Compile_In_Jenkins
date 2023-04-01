7c1b33ba4f6b963a455086f74c82774502838be3
mlakshmis <116376085+mlakshmis@users.noreply.github.com>
2023-04-01 23:44:58 +0530

    Delete SumOfNumbers.java

--- SumOfNumbers.java
+++
@@ -1,9 +0,0 @@
-public class SumOfNumbers  
-{  
-public static void main(String args[])   
-{  
-int n1 = 10, n2 = 10, sum;  
-sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
-}  
-}
